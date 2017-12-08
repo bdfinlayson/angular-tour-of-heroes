6563b67113ccdf8548710144825324fa0cea6f52
Bryan Finlayson <bryan.finlayson@metova.com>
2017-12-07 18:07:20 -0600

    show heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Bryan' },
+  { id: 2, name: 'Alex' },
+  { id: 3, name: 'John' },
+  { id: 4, name: 'Ray' },
+  { id: 5, name: 'Sam' },
+  { id: 6, name: 'Anessa' },
+  { id: 7, name: 'Rob' },
+  { id: 8, name: 'Eddie' },
+  { id: 9, name: 'Ned' },
+  { id: 10, name: 'Charles' }
+]
